56b3378ee3b72edb2130a3f760eaabee964c69c6
Alexander Marfut <sasha.marfut@gmail.com>
2015-05-18 14:27:26 +0300

    Add  AspNetUser model

---
+++ WebUI/DAL/Models/AspNetUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DAL.Models
+{
+    public class AspNetUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; }
+        public string SecurityStamp { get; }
+    }
+}
