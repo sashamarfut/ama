3c7de4604d614c985f9547f127b6a6d55bbc04f0
Alexander Marfut <sasha.marfut@gmail.com>
2015-05-18 14:27:57 +0300

    Make raiting column is not nullable

--- WebUI/DAL/Models/User.cs
+++ WebUI/DAL/Models/User.cs
@@ -5,7 +5,7 @@
         public int AspNetUserId { get; set; }
         public string Email { get; set; }
         public string Photo { get; set; }
-        public double? Raiting { get; set; }
+        public double Raiting { get; set; }
         public int AvailableLikes { get; set; }
         public int AddingCount { get; set; }        
     }
