6e73a6a261d0338236026ed17086361ea2e0e478
Alexander Marfut <sasha.marfut@gmail.com>
2015-05-25 18:42:01 +0300

    modify template

--- WebUI/WebUI/Scripts/app.js
+++ WebUI/WebUI/Scripts/app.js
@@ -1,29 +1,15 @@
-﻿var videoApp = angular.module('videoApp', []);
-
-
-videoApp.controller('VideoListCtrl', function ($scope, $http) {
-
-    $http.get('api/Video').success(function (data) {
-        $scope.videos = data;
-    });
-
+﻿var videoApp = angular.module('videoApp', ["ngRoute"]).
+    config(function ($routeProvider) {
+        $routeProvider.
+            when('/', { controller: VideoListCtrl, templateUrl: 'Templates/videoList.html' }).
+            otherwise({ redirectTo: '/' });
 });
 
 
-
-
-
-//var VideoApp = angular.module("VideoApp", ["ngRoute"]).
-//    config(function ($routeProvider) {
-//        $routeProvider.
-//            when('/', { controller: VideoList, template: '<h2>{{test}}</h2>' }).
-//            otherwise({ redirectTo: '/' });
-//    });
-
-
-//var VideoList = function ($scope, $location, $http) {
-    
-   
-//};
+var VideoListCtrl = function ($scope, $http) {
+    $http.get('api/Video').success(function (data) {
+         $scope.videos = data;
+    });
+};
 
 
