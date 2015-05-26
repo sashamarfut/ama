8393f4505a08950e79a759964c5dadc72cd6fe57
Alexander Marfut <sasha.marfut@gmail.com>
2015-05-26 14:36:20 +0300

    show video image

--- WebUI/WebUI/Mappers/VideoViewModelMapper.cs
+++ WebUI/WebUI/Mappers/VideoViewModelMapper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebUI.Infrastructure;
 using WebUI.Models;
 
 namespace WebUI.Mappers
--- WebUI/WebUI/Models/VideoViewModelBase.cs
+++ WebUI/WebUI/Models/VideoViewModelBase.cs
@@ -7,10 +7,19 @@ namespace WebUI.Models
 {
     public abstract class VideoViewModelBase
     {
+        public const string VIDEO_LOCATION_PATH = "/Content/Videos/";
+
         //Video
         public int VideoId { get; set; }
         public string Title { get; set; }
-        public string Url { get; set; }
+
+        private string url;
+        public string Url
+        {
+            get { return url; }
+            set { url = VIDEO_LOCATION_PATH + value; }
+        }
+        
         public int Views { get; set; }
         public int Likes { get; set; }
         public DateTime CreatedDate { get; set; }
