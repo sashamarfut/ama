3c2ecae1b16355607b0f14370f0daec9346a5272
Alexander Marfut <sasha.marfut@gmail.com>
2015-06-04 16:30:58 +0300

    separate lib frontend

--- FrontEnd/Scripts/app.js
+++ FrontEnd/Scripts/app.js
@@ -127,7 +127,6 @@ app.factory('Video', function ($resource) {
 
 app.factory('authService', ['$http', '$q', 'localStorageService', function ($http, $q, localStorageService) {
 
-    var serviceBase = 'http://localhost:51919/';
     var authServiceFactory = {};
 
     var _authentication = {
@@ -138,7 +137,7 @@ app.factory('authService', ['$http', '$q', 'localStorageService', function ($htt
     var _saveRegistration = function (registration) {
         _logOut();
 
-        return $http.post(serviceBase + 'api/account/register', registration).then(function (response) {
+        return $http.post('api/account/register', registration).then(function (response) {
             return response;
         });
     };
